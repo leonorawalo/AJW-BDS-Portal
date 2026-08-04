0f3fb4a121128d4c9fb8aacc83e1218b13421e59
leonorawalo <awaloleonora@gmail.com>
2026-08-04 14:22:19 +0300

    chore: creating a flutter app for Android building

---
+++ ajw_bds_app_flutter_v1/README.md
@@ -0,0 +1,3 @@
+# ajw_bds_app_flutter_v1
+
+A new Flutter project.
---
+++ ajw_bds_app_flutter_v1/android/app/src/main/kotlin/com/example/ajw_bds_app_flutter_v1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ajw_bds_app_flutter_v1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
